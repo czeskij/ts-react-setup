64b060d340779de391fec5813d87448a3f9eeff7
Jakub Rajski <jrajski@sii.pl>
2021-04-17 15:34:55 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from 'react';
+import React from 'react';
+
+export const App: FunctionComponent = (): JSX.Element => {
+    return <div>WORKIN' SHIZZ - test final!</div>;
+}
